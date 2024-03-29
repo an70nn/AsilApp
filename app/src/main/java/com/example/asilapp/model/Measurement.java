2c89d4211f7e793585f6d1203cf29206a8cdbe07
Andrea <perrinoa97@gmail.com>
2024-03-29 12:04:16 +0100

    Aggiunta classe Measurement (parametri medici)

---
+++ app/src/main/java/com/example/asilapp/model/Measurement.java
@@ -0,0 +1,49 @@
+package com.example.asilapp.model;
+
+import java.util.Date;
+
+public class Measurement {
+
+    private String tool;
+    private float value;
+    private String user;
+    private Date date;
+
+    public Measurement(String tool, float value, String user, Date date) {
+        this.tool = tool;
+        this.value = value;
+        this.user = user;
+        this.date = date;
+    }
+    public static Date setDateToday(){
+        return new Date();
+    }
+
+    public String getTool() {
+        return tool;
+    }
+    public void setTool(String tool) {
+        this.tool = tool;
+    }
+
+    public float getValue() {
+        return value;
+    }
+    public void setValue(float value) {
+        this.value = value;
+    }
+
+    public String getUser() {
+        return user;
+    }
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
