3232cdd091f6544b1ad61dd0aa633cf66001e9e8
Andrea <perrinoa97@gmail.com>
2024-05-07 16:32:18 +0200

    Aggiunto ResidenceFragment raggiungibile da HomeFragment per il centro di accoglienza

---
+++ app/src/main/java/com/example/asilapp/ui/ResidenceFragment.java
@@ -0,0 +1,22 @@
+package com.example.asilapp.ui;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import com.example.asilapp.R;
+
+public class ResidenceFragment extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_residence, container, false);
+        return view;
+    }
+
+}
